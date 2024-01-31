fe420b76c22a3dc752940085c00ee8aa9ca4f55d
Rostyslav Sheshenya <ross2004@iastate.edu>
2024-01-31 14:57:02 -0600

    New changes

---
+++ Experiment/Rostyslav_Experiments/android_unit1_2_intent/AndroidUnit1_2/app/src/main/java/com/example/androidexample/Main1Activity.java
@@ -0,0 +1,54 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.tomer.fadingtextview.FadingTextView;
+
+public class Main1Activity extends AppCompatActivity {
+
+    private FadingTextView openingMessage;
+
+    private TextView messageText;     // define message textview variable
+    private Button joinButton;// define counter button variable
+
+    String[] openingMessageText = {"Welcome", "to", "the", "fabulous", "Connect", "ONLINE!!!!!!!!!!"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);             // link to Main activity XML
+
+        /* initialize UI elements */
+        openingMessage = findViewById(R.id.openingMessage);
+        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        joinButton = findViewById(R.id.main_join_btn);// link to counter button in the Main activity XML
+
+        /* extract data passed into this activity from another activity */
+        Bundle extras = getIntent().getExtras();
+        if(extras == null) {
+            openingMessage.setTexts(openingMessageText);
+            messageText.setText("");
+        } else {
+            String number = extras.getString("NUM");  // this will come from LoginActivity
+            openingMessage.stop();
+            messageText.setText("The number of users online is " + number);
+        }
+
+        /* click listener on counter button pressed */
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when counter button is pressed, use intent to switch to Counter Activity */
+                Intent intent = new Intent(Main1Activity.this, CounterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- Experiment/Rostyslav_Experiments/android_unit1_2_intent/AndroidUnit1_2/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiment/Rostyslav_Experiments/android_unit1_2_intent/AndroidUnit1_2/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,47 +1,21 @@
 package com.example.androidexample;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import com.tomer.fadingtextview.FadingTextView;
 
-import org.w3c.dom.Text;
+public class MainActivity extends AppCompatActivity
+{
+    private FadingTextView openingMessage;
 
-public class MainActivity extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState)
+{
+super.onCreate(savedInstanceState);
 
-    private TextView messageText;     // define message textview variable
-    private Button joinButton;     // define counter button variable
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);             // link to Main activity XML
-
-        /* initialize UI elements */
-        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        joinButton = findViewById(R.id.main_join_btn);// link to counter button in the Main activity XML
-
-        /* extract data passed into this activity from another activity */
-        Bundle extras = getIntent().getExtras();
-        if(extras == null) {
-            messageText.setText("Counter Online");
-        } else {
-            String number = extras.getString("NUM");  // this will come from LoginActivity
-            messageText.setText("The number of users online is " + number);
-        }
-
-        /* click listener on counter button pressed */
-        counterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                /* when counter button is pressed, use intent to switch to Counter Activity */
-                Intent intent = new Intent(MainActivity.this, CounterActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+openingMessage = findViewById()
 }
+}
+
