6a3c48c632af462f59fa53dddc3bf5f1ac9af490
hnnebbe@iastate.edu <glpat-PeppZSfEt_dGywbas5i6>
2024-02-01 18:49:05 -0600

    feature: added new get request '/uptime', which shows amount of time the socket has been up

--- Experiment/Hudson_Experiments/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/Application.java
+++ Experiment/Hudson_Experiments/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/Application.java
@@ -3,6 +3,7 @@ package coms309;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 /**
  * PetClinic Spring Boot Application.
  * 
@@ -11,9 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+
     }
 
 }
