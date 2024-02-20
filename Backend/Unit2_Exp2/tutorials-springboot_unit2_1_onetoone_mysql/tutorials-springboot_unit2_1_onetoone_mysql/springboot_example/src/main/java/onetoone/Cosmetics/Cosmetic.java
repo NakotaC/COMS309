24744fb6d39771c8fc97e2afec330bfa662193c2
Hudson <glpat-PeppZSfEt_dGywbas5i6>
2024-02-20 17:45:42 -0600

    Started Backend Development: Base Unit2_2 Added CosmeticRepository

---
+++ Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Cosmetics/Cosmetic.java
@@ -0,0 +1,14 @@
+package onetoone.Cosmetics;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Cosmetic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+}
---
+++ Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Cosmetics/CosmeticController.java
@@ -0,0 +1,4 @@
+package onetoone.Cosmetics;
+
+public class CosmeticController {
+}
