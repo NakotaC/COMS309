0353603d2fc55775dad0ceedfe0bc991834ee160
Jack Olsan <glpat-1SRry-r-KJabyu-r1sdH>
2024-02-21 20:22:30 -0600

    Fully implemented the Monitor class

--- Experiment/Jack_Experiments/DEMO2/tutorials-springboot_unit2_2_onetomany/tutorials-springboot_unit2_2_onetomany/springboot_example/src/main/java/onetomany/Monitors/Monitor.java
+++ Experiment/Jack_Experiments/DEMO2/tutorials-springboot_unit2_2_onetomany/tutorials-springboot_unit2_2_onetomany/springboot_example/src/main/java/onetomany/Monitors/Monitor.java
@@ -12,4 +12,78 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import onetomany.Users.User;
 @Entity
 public class Monitor {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String name;
+    private String manufacturer;
+    private String resolution;
+    private int price;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    public Monitor(){
+
+    }
+
+    public Monitor( String manufacturer, String resolution, String name, int price) {
+        this.manufacturer = manufacturer;
+        this.resolution = resolution;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getResolution() {
+        return this.resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
+
