7ea690d0b2abb0c80cb731d5f2bae4160b18c422
Jack Olsan <glpat-1SRry-r-KJabyu-r1sdH>
2024-02-21 20:28:20 -0600

    Finished the controller and repository files for monitors

--- Experiment/Jack_Experiments/DEMO2/tutorials-springboot_unit2_2_onetomany/tutorials-springboot_unit2_2_onetomany/springboot_example/src/main/java/onetomany/Monitors/MonitorController.java
+++ Experiment/Jack_Experiments/DEMO2/tutorials-springboot_unit2_2_onetomany/tutorials-springboot_unit2_2_onetomany/springboot_example/src/main/java/onetomany/Monitors/MonitorController.java
@@ -2,6 +2,8 @@ package onetomany.Monitors;
 
 import java.util.List;
 
+import onetomany.Monitors.Monitor;
+import onetomany.Monitors.MonitorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,4 +14,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MonitorController {
+    @Autowired
+    MonitorRepository monitorRepository;
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+
+    @GetMapping(path = "/monitors")
+    List<Monitor> getAllMonitors(){
+        return monitorRepository.findAll();
+    }
+
+    @GetMapping(path = "/monitors/{id}")
+    Monitor getMonitorById( @PathVariable int id){
+        return monitorRepository.findById(id);
+    }
+
+    @PostMapping(path = "/monitors")
+    String createMonitor(Monitor monitor){
+        if (monitor == null)
+            return failure;
+        monitorRepository.save(monitor);
+        return success;
+    }
+
+    @PutMapping("/monitors/{id}")
+    Monitor updateMonitor(@PathVariable int id, @RequestBody Monitor request){
+        Monitor monitor = monitorRepository.findById(id);
+        if(monitor == null)
+            return null;
+        monitorRepository.save(request);
+        return monitorRepository.findById(id);
+    }
 }
--- Experiment/Jack_Experiments/DEMO2/tutorials-springboot_unit2_2_onetomany/tutorials-springboot_unit2_2_onetomany/springboot_example/src/main/java/onetomany/Monitors/MonitorRepository.java
+++ Experiment/Jack_Experiments/DEMO2/tutorials-springboot_unit2_2_onetomany/tutorials-springboot_unit2_2_onetomany/springboot_example/src/main/java/onetomany/Monitors/MonitorRepository.java
@@ -2,4 +2,5 @@ package onetomany.Monitors;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface MonitorRepository extends JpaRepository<Monitor, Long>  {
+    Monitor findById(int id);
 }
