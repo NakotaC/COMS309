6a0df0658b84d60f1cc1c0a3be6949e6bc67c8b7
Rostyslav Sheshenya <ross2004@iastate.edu>
2024-02-21 20:11:07 -0600

    Tried to create the main screen

--- Frontend/app/src/main/java/com/example/androidapp/MainActivity.java
+++ Frontend/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button1;
+    private Button clanButton;
+    private Button shopButton;
+    private TextView text1;
+    private ImageButton imageButton1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       // imageButton1 = findViewById(R.id.imageButton1);
+        text1 = findViewById(R.id.text1);
+        button1 = findViewById(R.id.button);
+        clanButton = findViewById(R.id.clanButton);
+        shopButton = findViewById(R.id.shopButton);
+
+
+
+
     }
 }
