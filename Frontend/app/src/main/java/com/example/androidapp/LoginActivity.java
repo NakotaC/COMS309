8ccf2a91ab78fbc702fc672345a6e87e067fe6b7
Nakota Clark <nakotac@iastate.edu>
2024-02-24 12:02:54 -0600

    Forgot to add back button functionality to login. Signup screen is now fully functional.

--- Frontend/app/src/main/java/com/example/androidapp/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/androidapp/LoginActivity.java
@@ -1,7 +1,5 @@
 package com.example.androidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,16 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -58,6 +52,13 @@ public class LoginActivity extends AppCompatActivity {
                 password = passwordEntry.getText().toString();
                 username = usernameEntry.getText().toString();
                 makeStringReq();
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
 
