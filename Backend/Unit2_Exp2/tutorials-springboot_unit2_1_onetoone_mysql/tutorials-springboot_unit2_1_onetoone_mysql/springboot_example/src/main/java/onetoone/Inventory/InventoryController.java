d435eb6c89700c34f21eef336ae91093e8909139
Jack Olsan <jrolsan@iastate.edu>
2024-04-25 15:59:09 -0500

    test

--- Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Inventory/InventoryController.java
+++ Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Inventory/InventoryController.java
@@ -26,6 +26,7 @@ public class InventoryController {
 
     //return all cosmetics in the game (in the DB) ???
     // ?
+    // ?
     @GetMapping(path = "/inventory")
     List<Inventory> getInventory() { return inventoryRepository.findAll(); }
 
