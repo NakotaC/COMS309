16980a2ad52261aee4d8f15acd3535f1ae4a152c
Jack Olsan <jrolsan@iastate.edu>
2024-04-28 17:45:11 -0500

    cicd testing

--- Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Game/GameController.java
+++ Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Game/GameController.java
@@ -41,7 +41,7 @@ public class GameController {
 //        int card = gameRepository.findById(gid).Draw();
 //        gameRepository.save(gameRepository.findById(gid));
 //        return card;
-//    }
+//    } fort
 
     @GetMapping(path = "/draw/str")
     String drawString(@RequestHeader("gameid") String gameid) throws JsonProcessingException {
