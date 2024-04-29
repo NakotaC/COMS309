04ae4da310c02bebf7dbfb75a68ee120ff5ff8bd
Hudson <hnnebbe@iastate.edu>
2024-04-29 13:26:33 -0500

    added new request clans/open to pull all open typed clans

--- Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Clans/ClanController.java
+++ Backend/Unit2_Exp2/tutorials-springboot_unit2_1_onetoone_mysql/tutorials-springboot_unit2_1_onetoone_mysql/springboot_example/src/main/java/onetoone/Clans/ClanController.java
@@ -32,6 +32,18 @@ public class ClanController {
         return clanRepository.findAll();
     }
 
+    @GetMapping(path = "/clans/open")
+    List<Clan> getAllOpenClans() {
+        ArrayList<Clan> openClans = new ArrayList<>();
+        for (int i = 1; i < clanRepository.count() + 1; i++) {
+            if (clanRepository.findById(i).getClanType().toLowerCase().equals("open")) {
+                System.out.print(clanRepository.findById(i).getClanType().toLowerCase());
+                openClans.add(clanRepository.findById(i));
+            }
+        }
+        return openClans;
+    }
+
     @GetMapping(path = "/clans/{clan_id}")
     Clan getClan(@PathVariable int clan_id) {
         return clanRepository.findById(clan_id);
@@ -120,6 +132,8 @@ public class ClanController {
         userRepository.save(user);
         return failure;
     }
+
+
 
     @PostMapping(path = "clan/Clan/cLAN/CLAM/CLam/cLAM")
     void initNoClan() {
